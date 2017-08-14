f1dab3273ad205005d2360f6791a4d1865d82236
WnRn <869530117@qq.com>
2017-08-14 10:33:59 +0800

    我添加了欢迎界面

---
+++ app/src/main/java/wr/bc/com/andrioidtext/WelcomeActivity.java
@@ -0,0 +1,9 @@
+package wr.bc.com.andrioidtext;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/8/14.
+ */
+public class WelcomeActivity extends AppCompatActivity{
+}
